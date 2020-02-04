dfd88d3bbfa4b0dd5006c0a664c12e94792f6f93
Melissa Martinez <58582650+melissmartinez@users.noreply.github.com>
2020-02-04 10:29:57 -0800

    Add files via upload

---
+++ boxing/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Create an empty List of type object
+            list<object> newObject = new List<object>();
+        //add the following values to the list: 7, 28, -1, true, "chair"
+            newObject.Add(7);
+            newObject.Add(28);
+            newObject.Add(-1);
+            newObject.Add(true);
+            newObject.Add("chair");
+        // Loop through the list and print all values (Hint: Type Inference might help here!)
+        // Add all values that are Int type together and output the sum
+            int sum = 0;
+            for (int i = 0; i < newObject.Count; i++)
+            {
+                if (newObject[i] is int) 
+                {
+                    Console.WriteLine($"This is an integer: {newObject[i]}");
+                    int ActualInt = (int)newObject[i];
+                    sum += ActualInt;
+                }
+                else if (newObject[i] is string) 
+                {
+                    Console.WriteLine($"This is an string: {newObject[i]}");
+                }
+                else if (newObject[i] is bool)
+                {
+                    Console.WriteLine($"This is an boolean: {newObject[i]}");
+                }
+            }
+            Console.WriteLine($"The sum of the integers is {sum}.");
+        }
+    }
+}
